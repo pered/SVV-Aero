a187537a8802cd5771ad20e66db46950ce440b0d
pered <47572304+pered@users.noreply.github.com>
2019-03-04 15:15:07 +0100

    Update momentcoefficientcalculator.m

---
+++ momentcoefficientcalculator.m
@@ -0,0 +1,10 @@
+forces = [2200,5.7,3.2]; %thrust force, position x, position y
+
+ow = [6000,4.5,3];
+
+coefficients = []
+
+geopos = [] %1st wing position x and y, 2nd horizontal stabiliser position x and y 
+
+
+
