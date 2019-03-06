87dcbf46b3680f47ba003a9944186e2e887dde5d
Zuren <47746150+sorinsky@users.noreply.github.com>
2019-03-06 16:25:19 +0100

    Update cgcomp.m

--- cgcomp.m
+++ cgcomp.m
@@ -1,7 +1,8 @@
 %function [ow,xcg,t] = cgcomp(bem,xcgbem,t,lfu,rfu,payload,fuelloaded)
 %xcgbem=292.18
 %ow=bem+payload
-momentarms=[131 131 214 214 251 251 288 288 170]
-payload=payloadfun()
-
-payload
+momentarms=[131 131 214 214 251 251 288 288 170];
+payload=payloadfun();
+fuel=fuelloaded-lfu-rfu;
+fuelmoment = fuelslope-
+fuelslope=2.8570048899755501222493887530562
