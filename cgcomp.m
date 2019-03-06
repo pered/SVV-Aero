1ac419305b4f8b3bf9e3cb95db40d88534ed0f7d
Zuren <47746150+sorinsky@users.noreply.github.com>
2019-03-06 16:34:11 +0100

    Update cgcomp.m

--- cgcomp.m
+++ cgcomp.m
@@ -4,5 +4,5 @@
 momentarms=[131 131 214 214 251 251 288 288 170];
 payload=payloadfun();
 fuel=fuelloaded-lfu-rfu;
-fuelmoment = fuelslope*(
-fuelslope=2.8570048899755501222493887530562
+fuelslope=2.8570048899755501222493887530562;
+fuelmoment = (fuelslope*(fuel-100)+298.16)*100; %inch-pounds
