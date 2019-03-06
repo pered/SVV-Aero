cb802144405a3a605014a0add6d92fdee8ae7444
Zuren <47746150+sorinsky@users.noreply.github.com>
2019-03-06 17:06:19 +0100

    Update cgcomp.m

--- cgcomp.m
+++ cgcomp.m
@@ -1,9 +1,13 @@
-%function [ow,xcg,t] = cgcomp(bem,xcgbem,t,lfu,rfu,payload,fuelloaded)
-%xcgbem=292.18
-%ow=bem+payload
-momentarms=[131 131 214 214 251 251 288 288 170];
-payload=payloadfun();
-fuel=fuelloaded-lfu-rfu;
-fuelslope=2.8570048899755501222493887530562;
-fuelmoment = (fuelslope*(fuel-100)+298.16)*100; %inch-pounds
-payloadmoment=momentarms*payload(:,2);
+function [ow,xcg,t] = cgcomp(bem,xcgbem,t,lfu,rfu,payload,fuelloaded)
+    %xcgbem=292.18
+    %luggage not included
+    momentarms=[131 131 214 214 251 251 288 288 170];
+    payload=payloadfun();
+    payloadmoment=momentarms*payload(:,2);
+    
+    fuel=fuelloaded-lfu-rfu;
+    fuelslope=2.8570048899755501222493887530562;
+    fuelmoment = (fuelslope*(fuel-100)+298.16)*100; %inch-pounds
+    
+    
+    ow=bem+sum(payload(:,2))+fuel
