083470de163c5439167caef194e86f18284250b9
pered <47572304+pered@users.noreply.github.com>
2019-03-06 11:57:10 +0100

    Update momentcoefficientcalculator.m

--- momentcoefficientcalculator.m
+++ momentcoefficientcalculator.m
@@ -29,9 +29,15 @@ weight = rampweight(1) - fuelused(index)
 
 cg = [(rampweight(1)*rampweight(2)-fuelused(index)*geopos(3,1))/weight(1),0]
 
-%Velocity of the aircraft
+%Velocity of the aircraft in knots
+tas = flightdata.Dadc1_tas.data(index)
 
-tas = flightdata.Dadc1_tas
+%Pressure and density in metric units
+
+alt = flightdata.Dadc1_alt.data(index)
+pressure = 101325*(288.15/(288.15-0.0019812*(alt)))^((9.80665*0.0289644)/(8.3144598*-0.0065))
+sat = flightdata.Dadc1_sat.data(index)
+rho = pressure/(8.3144598/0.0289644*(273.15+sat))
 
 
 
