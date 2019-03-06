f099e15076604f50b96f107b8b52f9f2011c3f84
Zuren <47746150+sorinsky@users.noreply.github.com>
2019-03-06 15:37:20 +0100

    Update cgcomp.m

--- cgcomp.m
+++ cgcomp.m
@@ -1,6 +1,7 @@
 %function [ow,xcg,t] = cgcomp(bem,xcgbem,t,lfu,rfu,payload,fuelloaded)
 %xcgbem=292.18
 %ow=bem+payload
+momentarms=[131 131 214 214 251 251 288 288 170]
 payload=payloadfun()
 
 payload
