60d93c9c855abed7ca6d74d38300d3ab20688259
pered <47572304+pered@users.noreply.github.com>
2019-03-12 14:16:53 +0100

    Update momentcoefficientcalculator.m

--- momentcoefficientcalculator.m
+++ momentcoefficientcalculator.m
@@ -2,8 +2,6 @@ thrust = [2200,5.7,3.2]; %thrust force, position x, position y
 
 rampweight = [12000,292,3]; %OW, x cg pos, z cg pos in ibs and inches
 
-coefficients = [0.21;0.04]; %1st Cl, 2nd Cd
-
 geopos = [[4.2, 2.7];[6.7,3.8];[285.56,0]]; %1st wing position x and y, 2nd horizontal stabiliser position x and y and fuel position in inches
 
 geospecs = [30,2.0569]; %in metric
@@ -16,7 +14,7 @@ geoposmetric = [[geopos(1,1)*0.0254, geopos(1,2)*0.0254];[geopos(2,1)*0.0254,geo
 
 %Time point in minutes since the powerup of the recording of data
 
-timeoffset = 30;
+timeoffset = 25+50/60;
 
 %Index of the start of the test
 
@@ -40,6 +38,10 @@ alt = flightdata.Dadc1_alt.data(index)
 pressure = 101325*(288.15/(288.15-0.0019812*(alt)))^((9.80665*0.0289644)/(8.3144598*-0.0065))
 sat = flightdata.Dadc1_sat.data(index)
 rho = pressure/(8.3144598/0.0289644*(273.15+sat))
+
+%Obtain coefficients of the aircraft
+
+coefficients = [0.21;0.04]; %1st Cl, 2nd Cd
 
 %
 
