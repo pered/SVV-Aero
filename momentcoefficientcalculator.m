c83dc9ff69340e34a0bc0fdebcb17a828bbbd7a8
pered <47572304+pered@users.noreply.github.com>
2019-03-20 09:36:14 +0100

    Update momentcoefficientcalculator.m

--- momentcoefficientcalculator.m
+++ momentcoefficientcalculator.m
@@ -1,17 +1,29 @@
-thrust = [2200,5.7,3.2]; %thrust force, position x, position y
+%Time point in minutes since the powerup of the recording of data
+
+%%%CG Shift Test%%%
+
+
+timeoffcgshift = [31 + 10/60,10/60] %End time and duration
+
+
+%%%Elevator Trim Tests%%%
+
+%Speedrun 1-7
+
+timeoffspeedrun = [37+19/60, 10/60; %End time and duration
+    0,0;
+    0,0;
+    0,0;
+    0,0;
+    0,0;
+    0,0]
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 geopos = [[4.2, 2.7];[6.7,3.8];[285.56,0]]; %1st wing position x and y, 2nd horizontal stabiliser position x and y and fuel position in inches
 
 geospecs = [30,2.0569]; %in metric, surface & mac
-
-%Time point in minutes since the powerup of the recording of data
-
-timeoffset = 19+50/60;
-duration = 1;
-
-timeoffsetcgshift = 31;
-duration = 1;
-
 
 bem=9165;
 xcgbem=292.18;
@@ -25,11 +37,19 @@ rampweight = [bem+fuelloaded+sum(payload(:,2)),xcgbem,3]; %OW, x cg pos, z cg po
 rampweightmetric = [rampweight(1)*0.45359237, rampweight(2)*0.0254, rampweight(3)*0.0254];
 geoposmetric = [[geopos(1,1)*0.0254, geopos(1,2)*0.0254];[geopos(2,1)*0.0254,geopos(2,2)*0.0254];[geopos(3,1)*0.0254, geopos(3,2)*0.0254]];
 
+
+
 %Index of the start of the test
 
-index = find(flightdata.Gps_utcSec.data >= flightdata.Gps_utcSec.data(1) + timeoffset*60);
-index = index(1);
-	;
+indexchshift = find(flightdata.Gps_utcSec.data >= flightdata.Gps_utcSec.data(1) - (timeoffcgshift(1,1)+timeoffcgshift(1,2))*60);
+
+
+indexspeedrun = find(flightdata.Gps_utcSec.data >= flightdata.Gps_utcSec.data(1) - (timeoffspeedrun(1,1)+timeoffspeedrun(1,2))*60);
+indexspeedrun = indexspeedrun(1);
+
+%Atmoshperic
+
+[alt,pressure,sat,rho,tas,eas] = atmoshperic(flightdata, index, indexend)
 
 %Weight obtaining the weight and cg at particular time point
 
@@ -40,20 +60,6 @@ weightmetric = rampweightmetric(1) - fuelused(index:indexend);
 [ow,xcg,t] = cgcomp(bem,xcgbem,index,flightdata.lh_engine_FU.data(index),flightdata.rh_engine_FU.data(index),payload,fuelloaded);
 
 
-%Velocity of the aircraft in m/s
-
-tas = flightdata.Dadc1_tas.data(index:indexend)*0.5144447;
-
-%Pressure and density in metric units
-
-alt = flightdata.Dadc1_alt.data(index:indexend);
-pressure = 101325.*(288.15./(288.15-0.0019812.*(alt))).^((9.80665*0.0289644)/(8.3144598*-0.0065));
-sat = flightdata.Dadc1_sat.data(index:indexend);
-rho = pressure./(8.3144598./0.0289644.*(273.15+sat));
-
-%Equivalent velocity of aircraft
-
-eas = tas.*sqrt(rho./1.225);
 
 
 
